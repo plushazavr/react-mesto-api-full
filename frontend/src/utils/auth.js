235ac7555b6f57afccc486db1d726691a9f88b08
Ekaterina Kazantseva <dr.kazantseva.ekaterina@gmail.com>
2022-08-04 21:22:15 +0300

    изменить api

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'https://auth.nomoreparties.co';
+export const BASE_URL = 'https://api.kazantseva.nomoredomains.sbs';
 
 const checkResponse = (response) => {
     if (response.ok) {
