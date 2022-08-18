6aed27bedbeb0d6a25d294b4894a08c60d072911
Ekaterina Kazantseva <dr.kazantseva.ekaterina@gmail.com>
2022-08-18 17:26:22 +0300

    rew3

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,4 +1,4 @@
-const { JWT_SECRET = 'dev-secret' } = process.env;
+const { NODE_ENV, JWT_SECRET } = process.env;
 const jwt = require('jsonwebtoken');
 const { UnauthorizedError } = require('../errors/401_unauthorized-error');
 
@@ -13,7 +13,7 @@ module.exports = (req, res, next) => {
   let payload;
 
   try {
-    payload = jwt.verify(token, JWT_SECRET);
+    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
   } catch (err) {
     throw new UnauthorizedError('Необходима авторизация');
   }
